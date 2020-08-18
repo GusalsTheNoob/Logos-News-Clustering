ef65b7bc558201e2f1901a6c4210cdbaeb201b2b
GusalsTheNoob <harrygusals@snu.ac.kr>
2020-08-18 21:01:24 +0900

    Style NewsAgency Schema
    Change a small typo at line 13.

--- logos/models/NewsAgency.py
+++ logos/models/NewsAgency.py
@@ -10,4 +10,4 @@ class NewsAgency(Document):
     agency_name = StringField(required=True)
     agency_url = StringField()
     news = ListField(ReferenceField(News), default=[])
-    authors = ListField(ReferenceFiled(Author), default=[])
+    authors = ListField(ReferenceField(Author), default=[])
